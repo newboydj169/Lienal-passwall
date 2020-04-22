4dccc7bc3ad6c84263c4c42c8594ac871ae1cdc4
newboydj169 <newboydj169@gmail.com>
2020-04-22 10:10:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,4 +12,4 @@ sed -i 's/192.168.1.251/192.168.50.5/g' package/base-files/files/bin/config_gene
 sed -i  s/'23552k(ubi),25600k@0x6c0000(firmware)'/'120832k(ubi),122880k@0x6c0000(firmware)'/ target/linux/ar71xx/image/legacy.mk
 
 # Add Lienol passwall packages.
-
+sed -i '1i\src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
